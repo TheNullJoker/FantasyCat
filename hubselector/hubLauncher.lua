5fe28a63a4c73811e007676a78a4d61314641f4a
TheNullJoker <107419771+TheNullJoker@users.noreply.github.com>
2024-01-24 22:43:33 +0100

    Update and rename hubSelector.lua to hubLauncher.lua

--- hubselector/hubLauncher.lua
+++ hubselector/hubLauncher.lua
@@ -40,76 +40,78 @@ function hubLauncher.on_loaded()
         modules.http:open(target_directory)
     end
 
+    
+
 end
 
 function hubLauncher.on_http_request( data )
-    if data["script"] ~= "HubLauncer.lua" then return end
+    if data["script"] ~= "hubLauncher.lua" then return end
 
     if data["path"] ~= "/luar" then return end
 
-    local activeScripts = json.decode(modules.session:api("getMember&scripts"))
-
-    local scriptIds = {}
-
-    -- Extract active scriptIDs.
-    for _, script in pairs(activeScripts.scripts) do
-        table.insert(ScriptIds, script.id)
-    end
+    local activeScripts = modules.scripts:get_loaded(false)
 
     -- Initialize flags as false.
     local ShatteredDreamsIsEnabled = false
     local CatsUniverseIsEnabled = false
     local LucidTermIsEnabled = false
 
-    -- Check if any hub is already enabled.
-    for _, targetId in pairs(hubLauncher.scriptIds) do
-        if id == "159" then
-           ShatteredDreamsIsEnabled = true
-        elseif id == "150" then
-        CatsUniverseIsEnabled = true
-        elseif id == "242" then
-            LucidTermIsEnabled = true
-        end
+    -- Extract active scriptIDs.
+    for _, script in pairs(activeScripts) do
+        
+        if script.id == "159" then
+            ShatteredDreamsIsEnabled = true
+         elseif script.id == "150" then
+         CatsUniverseIsEnabled = true
+         elseif script.id == "242" then
+             LucidTermIsEnabled = true
+         end
+        
+    end
+
+    if ShatteredDreamsIsEnabled or CatsUniverseIsEnabled or LucidTermIsEnabled then
+        fantasy.log("A hub is enabled")
+    else fantasy.log("no hub is enabled")
     end
 
     if data["params"]["button"] == "shatteredDreamsButton" then
         if CatsUniverseIsEnabled then
-            modules.session:api("toggleScriptStatus&id=150")
+            fantasy.session:api("toggleScriptStatus&id=150")
 
         elseif LucidTermIsEnabled then
-            modules.session:api("toggleScriptStatus&id=242")
+            fantasy.session:api("toggleScriptStatus&id=242")
         
         elseif not ShatteredDreamsIsEnabled then
-            modules.session:api("toggleScriptStatus&id=159")
+            fantasy.session:api("toggleScriptStatus&id=159")
         end
-        fantasy.log({}, "shaterredDreams Enabled")
+        fantasy.log("{}", "shaterredDreams Enabled")
         fantasy.scripts:reset( true )
 
     elseif data["params"]["button"] == "catsUniverseButton" then
-        if SchatteredDreamsIsEnabled then 
-            modules.session:api("toggleScriptStatus&id=159")  
+        if ShatteredDreamsIsEnabled then 
+            fantasy.session:api("toggleScriptStatus&id=159")  
 
         elseif LucidTermIsEnabled then
-            modules.session:api("toggleScriptStatus&id=242")
+            fantasy.session:api("toggleScriptStatus&id=242")
 
         elseif not CatsUniverseIsEnabled then
-            modules.session:api("toggleScriptStatus&id=150")
+            fantasy.session:api("toggleScriptStatus&id=150")
         end
-        fantasy.log({}, "CatsUniverse Enabled")
+        fantasy.log("{}", "CatsUniverse Enabled")
         fantasy.scripts:reset( true )
 
     elseif data["params"]["button"] == "lucidTermButton" then 
-        if SchatteredDreamsIsEnabled then 
-            modules.session:api("toggleScriptStatus&id=159")
+        if ShatteredDreamsIsEnabled then 
+            fantasy.session:api("toggleScriptStatus&id=159")
 
         elseif CatsUniverseIsEnabled then
-            modules.session:api("toggleScriptStatus&id=150")
+            fantasy.session:api("toggleScriptStatus&id=150")
             
         elseif not LucidTermIsEnabled then
-            modules.session:api("toggleScriptStatus&id=242")
+            fantasy.session:api("toggleScriptStatus&id=242")
 
         end
-        fantasy.log({}, "LucidTerm enabled")
+        fantasy.log("{}", "LucidTerm enabled")
         fantasy.scripts:reset( true )
     end
 end
