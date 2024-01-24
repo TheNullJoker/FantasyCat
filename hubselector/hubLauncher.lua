b1ff57a73f86e3b167e1b28da6f80cd1eb69b791
TheNullJoker <107419771+TheNullJoker@users.noreply.github.com>
2024-01-24 23:47:33 +0100

    Update hubLauncher.lua

--- hubselector/hubLauncher.lua
+++ hubselector/hubLauncher.lua
@@ -14,7 +14,7 @@ local json = require("json")
 
 local hubLauncher = {
 
-    auto_update = false,
+    auto_update = true,
 
     auto_open= true,
 }
@@ -23,97 +23,84 @@ local hubLauncher = {
 
 function hubLauncher.on_loaded()
 
-    local target_directory = fantasy.fmt("{}/constellation4/resources/hubSelector.html", modules.file:current_directory())
+    local target_directory = fantasy.fmt("{}/constellation4/resources/hubLauncher.html", modules.file:current_directory())
 
         target_directory = modules.file:fix_separators(target_directory)
 
     if hubLauncher.auto_update then
-        
 
-        modules.http:to_file("https://raw.githubusercontent.com/TheNullJoker/FantasyCat/main/hubselector/hubselector.html", target_directory)
 
-        fantasy.log("hubSelector.html downloaded to {}", target_directory)
-      
+        modules.http:to_file("https://raw.githubusercontent.com/TheNullJoker/FantasyCat/main/hubselector/hubLauncher.html", target_directory)
+
+        fantasy.log("hubLauncher.html downloaded to {}", target_directory)
+
     end
 
     if hubLauncher.auto_open then
         modules.http:open(target_directory)
     end
 
-    
-
 end
 
 function hubLauncher.on_http_request( data )
-    if data["script"] ~= "hubLauncher.lua" then return end
+    if data["script"] ~= "hubLauncher.lua" or data["path"] ~= "/luar" then return end
 
-    if data["path"] ~= "/luar" then return end
+
 
     local activeScripts = modules.scripts:get_loaded(false)
 
     -- Initialize flags as false.
-    local ShatteredDreamsIsEnabled = false
-    local CatsUniverseIsEnabled = false
-    local LucidTermIsEnabled = false
+    local scriptFlags = {
+        ShatteredDreams = false,
+        CatsUniverse = false,
+        LucidTerm = false
+    }
 
     -- Extract active scriptIDs.
     for _, script in pairs(activeScripts) do
-        
-        if script.id == "159" then
+        --fantasy.log(script.name)
+        if script.name == "shattered_dreams.lua" then
+            --fantasy.log("shattered_dreams.lua is enabled")
             ShatteredDreamsIsEnabled = true
-         elseif script.id == "150" then
+         elseif script.name == "cats_universe.lua" then
+            --fantasy.log("catus_universe.lua is enabled")
          CatsUniverseIsEnabled = true
-         elseif script.id == "242" then
+         elseif script.name == "lucid_term.lua" then
+            --fantasy.log("lucid_term.lua is enabled")
              LucidTermIsEnabled = true
          end
-        
+
     end
 
-    if ShatteredDreamsIsEnabled or CatsUniverseIsEnabled or LucidTermIsEnabled then
-        fantasy.log("A hub is enabled")
-    else fantasy.log("no hub is enabled")
+    if CatsUniverseIsEnabled then
+        fantasy.session:api("toggleScriptStatus&id=150")
+
+    elseif LucidTermIsEnabled then
+        fantasy.session:api("toggleScriptStatus&id=242")
+
+    elseif ShatteredDreamsIsEnabled then
+        fantasy.session:api("toggleScriptStatus&id=159")
     end
 
-    if data["params"]["button"] == "shatteredDreamsButton" then
-        if CatsUniverseIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=150")
-
-        elseif LucidTermIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=242")
-        
-        elseif not ShatteredDreamsIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=159")
-        end
-        fantasy.log("{}", "shaterredDreams Enabled")
-        fantasy.scripts:reset( true )
-
-    elseif data["params"]["button"] == "catsUniverseButton" then
-        if ShatteredDreamsIsEnabled then 
-            fantasy.session:api("toggleScriptStatus&id=159")  
-
-        elseif LucidTermIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=242")
-
-        elseif not CatsUniverseIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=150")
-        end
-        fantasy.log("{}", "CatsUniverse Enabled")
-        fantasy.scripts:reset( true )
-
-    elseif data["params"]["button"] == "lucidTermButton" then 
-        if ShatteredDreamsIsEnabled then 
-            fantasy.session:api("toggleScriptStatus&id=159")
-
-        elseif CatsUniverseIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=150")
-            
-        elseif not LucidTermIsEnabled then
-            fantasy.session:api("toggleScriptStatus&id=242")
-
-        end
-        fantasy.log("{}", "LucidTerm enabled")
-        fantasy.scripts:reset( true )
+    local function toggleScriptStatus(scriptID)
+        fantasy.session:api("toggleScriptStatus&id=" .. scriptID)
     end
+
+    local buttons = {
+        shatteredDreamsButton = 159,
+        catsUniverseButton = 150,
+        lucidTermButton = 242
+    }
+
+    local button = data["params"]["button"]
+    local selectedButton = buttons[button]
+
+    if selectedButton then
+        toggleScriptStatus(selectedButton)
+        fantasy.log("{}", button .. " Enabled")
+        modules.scripts:reset( true )
+    end
+
 end
 
 return hubLauncher
